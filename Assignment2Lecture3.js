f41cfba8b32c2a1ebe5cf41ded34f9de6dcfc7e4
matthewgroff <groff@Matthews-MacBook-Pro-2.local>
2015-10-25 14:15:47 -0700

    Create a basic calculator capable of alerting results.

---
+++ Assignment2Lecture3.js
@@ -0,0 +1,32 @@
+var x = 20
+var y = 10
+
+function add(x,y){
+	alert(x + y);
+};
+
+
+function subtract(x,y){
+	alert(x - y);
+
+};
+
+function multiply(x,y){
+	alert(x * y);
+
+};
+function divide(x,y){
+	alert(x / y);
+
+};
+
+add(x,y);
+subtract(x,y);
+divide(x,y);
+multiply(x,y);
+
+
+
+
+
+
