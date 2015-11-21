a04daf3c873cc4cabafbfe71005722686b582f69
matthewgroff <groff@Matthews-MacBook-Pro-2.local>
2015-11-20 21:48:08 -0800

    …

---
+++ assignment1lecture5.js
@@ -0,0 +1,24 @@
+<script
+src=
+"http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.
+min.js">
+</script>
+
+<script>
+$(document).ready(function(){
+	$('button').click(function(){
+        $("h1").hide()
+	});
+};
+
+</script>
+
+<body>
+<h1>abc</h1>
+<div>
+<button>click here</button>
+ <h1>div-1</h1>
+ <h1>div-2</h1>
+ </div>
+ <h1>xyz</h1>
+</body>
